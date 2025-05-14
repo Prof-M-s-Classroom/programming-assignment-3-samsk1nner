e91a484cc28718b6cc5e85a73522ddf6d03cb42d
samsk1nner <sskinner.mb@gmail.com>
2025-05-13 17:35:05 -0700

    Constructors, Pseudo, Understanding

--- graph.h
+++ graph.h
@@ -3,7 +3,16 @@
 
 class Graph {
 public:
-    Graph(int vertices);
+    Graph(int vertices){
+        numVertices = vertices;
+        adjMatrix = new int*[vertices];
+        for(int i = 0; i < vertices; i++){
+            adjMatrix[i] = new int[vertices];
+            for(int j = 0; j < vertices; j++){
+                adjMatrix[i][j] = 0; // all verticies intialized with 0 edges.
+            }
+        }
+    }
     ~Graph();
     void addEdge(int u, int v, int weight);
     void primMST();  // Must print MST edges and total weight
--- heap.h
+++ heap.h
@@ -3,13 +3,33 @@
 
 class MinHeap {
 public:
-    MinHeap(int capacity);
-    ~MinHeap();
-    void insert(int vertex, int key);
-    int extractMin();
-    void decreaseKey(int vertex, int newKey);
-    bool isInMinHeap(int vertex);
-    bool isEmpty();
+    MinHeap(int capacity){
+        heapArray = new int[capacity + 1];
+        keyArray = new int[capacity + 1];
+        position = new int[capacity + 1];
+        this->capacity = capacity;
+        size = 0;
+    }
+    ~MinHeap(){
+        delete[] heapArray;
+        delete[] keyArray;
+        delete[] position;
+    }
+
+    void insert(int vertex, int key); // add vertex v with key into the heap.
+    // insert at the end of heap array, filter up, then update postion
+
+    int extractMin(); // remove + return the min node
+
+    void decreaseKey(int vertex, int newKey); // set new key for vertex v and update position
+
+    bool isInMinHeap(int vertex){ //  boolena, return true if vertex v is in heap
+        return position[vertex] != -1;
+    }
+
+    bool isEmpty() {  // boolean, return size of 0 for no nodes present.
+        return size == 0; 
+    } 
 
 private:
     int* heapArray;        // Heap of vertex indices
